3c1bc487668c8c4a5661e12ca1ea3f551276749d
Divyesh230902 <pateldivyesh2309@gmail.com>
2022-03-05 09:56:03 +0530

    Aryan Task-3 Done

--- task 3 git hub.py
+++ task 3 git hub.py
@@ -4,13 +4,12 @@ CREATED ON:4/03/2022
 TASK:SWAPPING A=10,B-20 TO A=20,B=10
 
 '''
+#UDF to swap two varaibles
+def swaptwo(x,y):
+    x,y = y,x #swap logig
+    print("Values after swapping:\n{x},{y}") #this will print swaped values
 
-
-
-a=10
-b=20
-#SWAPPING
-a,b = b,a
-
-#OUT PUT
-print(a,b)
+a=10 #var-1
+b=20 #var-2
+print("Values before swapping:\n{x},{y}") #this will print original values
+swaptwo(a,b) #swap function used.
